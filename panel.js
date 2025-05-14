42ab08442bec714a2b3b12a40c0ae2c68ae5eb13
Gabriel-Pacheco-Martinez <gabriel@Gabriels-MacBook-Air.local>
2025-05-14 19:28:51 -0400

    Request content extracted

--- panel.js
+++ panel.js
@@ -1,7 +1,23 @@
 const requestList = document.getElementById('request-list');
 
-chrome.devtools.network.onRequestFinished.addListener((request) => {
-  const li = document.createElement('li');
-  li.textContent = `${request.request.method} ${request.request.url}`;
-  requestList.appendChild(li);
-});
+chrome.devtools.network.onRequestFinished.addListener(
+  function(request) {
+    // Construct JSON object
+    let data = {
+      method: request.request.method,
+      url: request.request.url,
+      headers: request.request.headers
+    }
+
+    // Log the JSON object
+    console.log(JSON.stringify(data, null, 2));
+
+    // Create a list item and append JSON
+    const pre = document.createElement('pre');
+    pre.textContent = JSON.stringify(data, null, 2);
+
+    const li = document.createElement('li');
+    li.appendChild(pre);
+    requestList.appendChild(li);
+  }
+);
